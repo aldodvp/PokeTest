b9daae8012c88f7122cdfbdb809f5dfc6d5e3738
aldo <35746612+aldodvp@users.noreply.github.com>
2020-09-29 12:50:35 -0500

    Add files via upload

---
+++ gsheets.py
@@ -0,0 +1,57 @@
+import os
+from Google import Create_Service
+import requests
+
+poks = [
+    ['ID', 'Name', 'URL']
+]
+
+def pokedex(url='https://pokeapi.co/api/v2/pokemon/', offset=0):
+    args = {'offset': offset} if offset else {}
+  
+    response = requests.get(url, params=args)
+    if response.status_code == 200:
+    
+        payload = response.json()
+        results = payload.get('results', [])
+
+        if results:
+            for pokemon in results:
+                url = pokemon ['url']
+                temp = url.split("/")
+                id= temp[6]
+                name = pokemon['name']
+                poks.append([id,name,url])
+                print(id, name, url )
+
+        next = input("Next page? [Y/N]")
+        if next == ('y'):
+            pokedex(offset=offset+20)
+ 
+if __name__ == '__main__':
+    url =  'https://pokeapi.co/api/v2/pokemon/'
+    pokedex()
+    CLIENT_SECRET_FILE = 'test.json'
+    API_NAME = 'sheets'
+    API_VERSION = 'v4'
+    SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+    
+    service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
+
+    spreadsheet_id = '1fGlYK--7-XzaJpgT9Bih6MVKo_f5PXv37chn_F4WQSs'
+
+
+    cell_range_insert = 'A1'
+    value_range_body = {
+        'majorDimension': 'ROWS',
+        'values': poks
+    }
+    
+    service.spreadsheets().values().update(
+        spreadsheetId=spreadsheet_id,
+        valueInputOption='USER_ENTERED',
+        range=cell_range_insert,
+        body=value_range_body
+    ).execute()
+    
+
